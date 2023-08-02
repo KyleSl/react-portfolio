c43893ced129cd17f87b09de37a10e1e60ed3584
Kyle <kyleslaughter8@gmail.com>
2023-08-02 06:00:07 -0600

    finished navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Navbar from "./components/Navbar";
+import { useState } from "react";
 import './App.css';
 
 function App () {
+  const [ activePage, updatePage ] = useState('About Me');
+  const pageNames = ['About Me', 'Portfolio', 'Contact', 'Resume'];
+
   return (
     <>
-      <Navbar />
+      <Navbar pageNames={pageNames} activePage={activePage} onSelectItem={updatePage}/>
     </>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,23 @@
+interface Props {
+    pageNames: string[];
+    activePage: string;
+    onSelectItem: ( item: string ) => void;
+}
 
-
-export default function Navbar () {
+export default function Navbar ({ pageNames, activePage, onSelectItem }: Props) {
     return (
         <nav className="navbar navbar-expand-lg custom-bg-purple">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Kyle Slaughter</a>
+                <a className="navbar-brand">Kyle Slaughter</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <a className="nav-link active" aria-current="page" href="#">About Me</a>
-                        <a className="nav-link" href="#">Portfolio</a>
-                        <a className="nav-link" href="#">Contact</a>
-                        <a className="nav-link" href="#">Resume</a>
+                        {pageNames.map( (item, index ) => (<li key={index} 
+                        className={ item === activePage ? 'nav-link active' : 'nav-link'}
+                        onClick={() => { onSelectItem(item) }}
+                        >{item}</li>))}
                     </div>
                 </div>
             </div>
