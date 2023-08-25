cb77fd61b4ec5a44b307a7924f75ed1959707d13
Kyle <kyleslaughter8@gmail.com>
2023-08-25 17:22:02 -0600

    added linkedin page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer () {
     return (
         <footer className="custom-bg-purple mt-2 p-2">
-            <p className="text-center">GitHub: <a className="text-info" href="https://github.com/KyleSl">KyleSl</a>, LinkedIn: N/A, StackOverflow: <a className="text-info" href="https://stackoverflow.com/users/22327779/smeltedbutter">smeltedbutter</a></p>
+            <p className="text-center">GitHub: <a className="text-info" href="https://github.com/KyleSl">KyleSl</a>, LinkedIn: <a className="text-info" href="https://www.linkedin.com/in/kyle-slaughter-934b7b244/">Kyle Slaughter</a>, StackOverflow: <a className="text-info" href="https://stackoverflow.com/users/22327779/smeltedbutter">smeltedbutter</a></p>
             <p className="text-center">Made using Bootstrap, React, and Vite</p>
         </footer>
     );
